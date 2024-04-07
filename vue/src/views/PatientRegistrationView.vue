d96c36c2d012b15ac7e6763896d373d04d660d9b
Shelby Bittner <bittnershelby@gmail.com>
2024-04-07 18:44:39 -0400

    Added grid, flex display qualities, and minimal styling in CSS

--- vue/src/views/PatientRegistrationView.vue
+++ vue/src/views/PatientRegistrationView.vue
@@ -4,48 +4,74 @@
         <div id="patient-registration-component">
             <h1 id="patient-registration-header">Patient Registration</h1>
 
-            <label>First Name</label>
-            <div class="form-input-group">
-                <input type="text" id="first-name-input-box" v-model="patient.firstName" required autofocus />
+            <div id="name-flex-box">
+                <div id="first-name">
+                    <header>First Name</header>
+                    <div class="form-input-group">
+                        <input type="text" id="first-name-input-box" v-model="patient.firstName" required autofocus />
+                    </div>
+                </div>
+
+                <div id="last-name">
+                    <header>Last Name</header>
+                    <div class="form-input-group">
+                        <input type="text" id="last-name-input-box" v-model="patient.lastName" required autofocus />
+                    </div>
+                </div>
             </div>
 
-            <label>Last Name</label>
-            <div class="form-input-group">
-                <input type="text" id="last-name-input-box" v-model="patient.lastName" required autofocus />
+
+            <div id="address-flex-box">
+                <header>Address</header>
+                <div class="form-input-group">
+                    <input type="text" id="address1-input-box" v-model="patient.addressLineOne" required autofocus />
+                </div>
+                <div class="form-input-group">
+                    <input type="text" id="address2-input-box" v-model="patient.addressLineTwo" required autofocus />
+                </div>
             </div>
 
-            <label>Address</label>
-            <div class="form-input-group">
-                <input type="text" id="address1-input-box" v-model="patient.addressLineOne" required autofocus />
-            </div>
-            <div class="form-input-group">
-                <input type="text" id="address2-input-box" v-model="patient.addressLineTwo" required autofocus />
+
+            <div id="city-state-zip-flex-box">
+                <div id="city">
+                    <header>City</header>
+                    <div class="form-input-group">
+                        <input type="text" id="city-input-box" v-model="patient.city" required autofocus />
+                    </div>
+                </div>
+
+                <div id="state">
+                    <header>State</header>
+                    <div class="form-input-group">
+                        <input type="text" id="state-input-box" v-model="patient.state" required autofocus />
+                    </div>
+                </div>
+
+                <div id="zip-code">
+                    <header>Zip Code</header>
+                    <div class="form-input-group">
+                        <input type="text" id="zipcode-input-box" v-model="patient.zipCode" required autofocus />
+                    </div>
+                </div>
             </div>
 
-            <label>City</label>
-            <div class="form-input-group">
-                <input type="text" id="city-input-box" v-model="patient.city" required autofocus />
+
+            <div id="contact-flex-box">
+                <div id="phone-number">
+                    <header>Phone Number</header>
+                    <div class="form-input-group">
+                        <input type="text" id="phone-number-input-box" v-model="patient.phoneNumber" required autofocus />
+                    </div>
+                </div>
+
+                <div id="email-address">
+                    <header>Email Address</header>
+                    <div class="form-input-group">
+                        <input type="text" id="email-address-input-box" v-model="patient.emailAddress" required autofocus />
+                    </div>
+                </div>
             </div>
 
-            <label>State</label>
-            <div class="form-input-group">
-                <input type="text" id="state-input-box" v-model="patient.state" required autofocus />
-            </div>
-
-            <label>Zip Code</label>
-            <div class="form-input-group">
-                <input type="text" id="zipcode-input-box" v-model="patient.zipCode" required autofocus />
-            </div>
-
-            <label>Phone Number</label>
-            <div class="form-input-group">
-                <input type="text" id="phone-number-input-box" v-model="patient.phoneNumber" required autofocus />
-            </div>
-
-            <label>Email Address</label>
-            <div class="form-input-group">
-                <input type="text" id="email-address-input-box" v-model="patient.emailAddress" required autofocus />
-            </div>
 
             <button id="button" type="submit" v-on:click="submitPatientForm()">Submit</button>
 
@@ -83,4 +109,95 @@ export default {
 </script>
 
 
-<style></style>
+<style>
+.container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+        "header"
+        "name"
+        "address"
+        "city-state-zip"
+        "contact"
+        "button"
+    ;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+#patient-registration-header {
+    grid-area: header;
+    display: flex;
+    text-transform: uppercase;
+}
+
+#patient-registration-component {
+    display: flex;
+    flex-wrap: wrap;
+    background-color: rgb(201, 201, 201, 0.5);
+    justify-content: center;
+    align-items: center;
+}
+
+#name-flex-box {
+    grid-area: name;
+    display: flex;
+    flex-basis: 100%;
+    justify-content: space-evenly;
+}
+
+#address-flex-box {
+    grid-area: address;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+#city-state-zip-flex-box {
+    grid-area: city-state-zip;
+    display: flex;
+    flex-basis: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+#contact-flex-box {
+    grid-area: contact;
+    display: flex;
+    flex-basis: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+#button {
+    grid-area: button;
+    justify-content: center;
+    align-items: center;
+}
+
+label {
+    padding: 5pt;
+    display: block;
+}
+
+input {
+    height: 15px;
+    padding: 7px;
+    border-style: none;
+    border-radius: 3px;
+    outline: none;
+}
+
+#first-name-input-box {
+    width: 250px;
+}
+
+#last-name-input-box {
+    width: 250px;
+}
+
+#address1-input-box {
+    width: 700px;
+}
+</style>
