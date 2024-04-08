6a60362cf4601e656aa843aeae4936b57184498c
Vivek Dindi <vivekcsm19@gmail.com>
2024-04-08 12:06:30 -0400

    compleated patient API

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,46 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+DROP TABLE IF EXISTS patient;
+
+CREATE TABLE IF NOT EXISTS patient
+(
+    patient_id SERIAL,
+    user_id int,
+    first_name VARCHAR (64),
+    last_name VARCHAR (64) ,
+	phone_number VARCHAR(15),
+    email VARCHAR(64),
+	email_reminder BOOLEAN,
+	address_line_1 VARCHAR (128) ,
+	address_line_2   VARCHAR (128),
+	city  VARCHAR (64),
+	state VARCHAR (64),
+	zip_code VARCHAR (15),
+	CONSTRAINT PK_patient PRIMARY KEY (patient_id)
+
+);
+
+
+DROP TABLE IF EXISTS doctor;
+
+CREATE TABLE IF NOT EXISTS doctor
+(
+        doctor_id SERIAL,
+        user_id int,
+        first_name VARCHAR (64),
+        last_name VARCHAR (64) ,
+    	phone_number VARCHAR(15),
+        email VARCHAR(64),
+    	email_reminder BOOLEAN,
+    	address_line_1 VARCHAR (128) ,
+    	address_line_2   VARCHAR (128),
+    	city  VARCHAR (64),
+    	state VARCHAR (64),
+    	zip_code VARCHAR (15),
+    	CONSTRAINT PK_doctor PRIMARY KEY (doctor_id)
+);
+
+
 COMMIT TRANSACTION;
+
--- java/src/main/java/com/techelevator/controller/DoctorController.java
+++ java/src/main/java/com/techelevator/controller/DoctorController.java
@@ -1,40 +1,40 @@
-package com.techelevator.controller;
-import com.techelevator.dao.DoctorDao;
-import com.techelevator.exception.DaoException;
-import com.techelevator.model.*;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import com.techelevator.dao.UserDao;
-import com.techelevator.security.jwt.JWTFilter;
-import com.techelevator.security.jwt.TokenProvider;
-
-@PreAuthorize("permitAll")
-@RestController
-@CrossOrigin
-public class DoctorController {
-    private DoctorDao doctorDao;
-    public DoctorController(DoctorDao doctorDao) {
-        this.doctorDao = doctorDao;
-    }
-
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(path = "/doctor", method = RequestMethod.POST)
-    public void createTransfer(@RequestBody Doctor newdoctor) {
-        try {
-            doctorDao.createDoctor( doctorDao);
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , "doctor API test failed.");
-        }
-    }
-}
+//package com.techelevator.controller;
+//import com.techelevator.dao.DoctorDao;
+//import com.techelevator.exception.DaoException;
+//import com.techelevator.model.*;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import com.techelevator.dao.UserDao;
+//import com.techelevator.security.jwt.JWTFilter;
+//import com.techelevator.security.jwt.TokenProvider;
+//
+//@PreAuthorize("permitAll")
+//@RestController
+//@CrossOrigin
+//public class DoctorController {
+//    private DoctorDao doctorDao;
+//    public DoctorController(DoctorDao doctorDao) {
+//        this.doctorDao = doctorDao;
+//    }
+//
+//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @RequestMapping(path = "/doctor", method = RequestMethod.POST)
+//    public void createTransfer(@RequestBody Doctor newdoctor) {
+//        try {
+//            doctorDao.createDoctor( doctorDao);
+//        } catch (Exception ex) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , "doctor API test failed.");
+//        }
+//    }
+//}
