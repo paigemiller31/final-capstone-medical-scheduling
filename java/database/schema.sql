538045177f45c6162bf59e3a1dce19cfd438527a
Vivek Dindi <vivekcsm19@gmail.com>
2024-04-11 19:15:38 -0400

    updated schema for office

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
 
-DROP TABLE IF EXISTS office, patient, doctor, users CASCADE;
+DROP TABLE IF EXISTS  patient, doctor, office, users CASCADE;
 	
 
 CREATE TABLE users (
@@ -12,6 +12,21 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE IF NOT EXISTS office
+(
+	office_id SERIAL,
+	office_name VARCHAR (64),
+	address_line_1 VARCHAR (128) ,
+	address_line_2   VARCHAR (128),
+	city  VARCHAR (64),
+	state VARCHAR (64),
+	zip_code VARCHAR (15),
+	phone_number VARCHAR(15),
+	email VARCHAR(64),
+	office_hours VARCHAR(15),
+	CONSTRAINT PK_office PRIMARY KEY (office_id)
+
+	);
 
 CREATE TABLE IF NOT EXISTS patient
 (
@@ -32,30 +47,17 @@ CREATE TABLE IF NOT EXISTS patient
 CREATE TABLE IF NOT EXISTS doctor
 (
     doctor_id int,
-    office_id int UNIQUE,
+    office_id int ,
     first_name VARCHAR (64),
     last_name VARCHAR (64) ,
 	specialization VARCHAR (64),
 	cost_per_hour int,
 	CONSTRAINT PK_doctor PRIMARY KEY (doctor_id),
-    CONSTRAINT FK_doctor_users FOREIGN KEY (doctor_id) REFERENCES users (user_id)
+    CONSTRAINT FK_doctor_users FOREIGN KEY (doctor_id) REFERENCES users (user_id),
+    CONSTRAINT FK_doctor_office FOREIGN KEY (office_id) REFERENCES office (office_id)
 );
 
-CREATE TABLE IF NOT EXISTS office
-(
-	office_id SERIAL,
-	office_name VARCHAR (64),
-	address_line_1 VARCHAR (128) ,
-	address_line_2   VARCHAR (128),
-	city  VARCHAR (64),
-	state VARCHAR (64),
-	zip_code VARCHAR (15),
-	phone_number VARCHAR(15),
-	email VARCHAR(64),
-	office_hours VARCHAR(15),
-	CONSTRAINT PK_office PRIMARY KEY (office_id),
-	CONSTRAINT FK_office_doctor FOREIGN KEY (office_id) REFERENCES doctor (office_id)
-	);
+
 
 COMMIT TRANSACTION;
 
