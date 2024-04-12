59a0106c2acc738de04949221292384a6b2003b6
Jasmine Alries <alriesjasmine@gmail.com>
2024-04-12 12:51:22 -0400

    fixed jdbcdoctordao and implemented missing methods

--- java/src/main/java/com/techelevator/model/Doctor.java
+++ java/src/main/java/com/techelevator/model/Doctor.java
@@ -8,6 +8,13 @@ public class Doctor {
     private String lastName ;
     private String specialization;
     private int costPerHour;
+    private int phoneNumber;
+    private String email;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private int zipCode;
 
     public Doctor() {
 
@@ -20,6 +27,13 @@ public class Doctor {
         this.lastName = lastName;
         this.specialization = specialization;
         this.costPerHour = costPerHour;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
     }
 
     public int getDoctorId() {
@@ -70,4 +84,31 @@ public class Doctor {
         this.costPerHour = costPerHour;
     }
 
+    public Object getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Object getEmail() {
+        return email;
+    }
+
+    public Object getAddressLine1() {
+        return addressLine1;
+    }
+
+    public Object getAddressLine2() {
+        return addressLine2;
+    }
+
+    public Object getCity() {
+        return city;
+    }
+
+    public Object getState() {
+        return state;
+    }
+
+    public Object getZipCode() {
+        return zipCode;
+    }
 }
