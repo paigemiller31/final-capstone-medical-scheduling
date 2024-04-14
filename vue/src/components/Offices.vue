5a1c3e2e1d01c4601978fa80a35a6e89c6a1a37c
Paige Miller <paige7390@gmail.com>
2024-04-13 23:46:54 -0400

    Finished the patient registration page and attempted function/retrieval of individual office details

---
+++ vue/src/components/Offices.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        
+    </div>
+    
+</template>
+    
+<script>
+import OfficeService from '../services/OfficeService';
+
+export default {
+props: {
+  office: Object,
+},
+
+};
+</script>
+    
+<style></style>
--- vue/src/views/OfficesView.vue
+++ vue/src/views/OfficesView.vue
@@ -1,22 +1,24 @@
 <template>
-    <div>       
-         <div v-for="o in this.officeList" v-bind:key="o.id" > "This is a office: "  {{o}}  </div>    
-    
-         <div v-for="d in this.doctorList" v-bind:key="d.id" >  "This is a doctor: " {{d}}  </div>
+    <div>
+        
+        <div v-for="office in this.officeList" v-bind:key="office.id"> "This is a office: " {{ office }} </div>
 
-         <h1  v-if="$store.state.user.authorities[0].name ===  'ROLE_DOCTOR'"  > Only doctor can Update </h1>
-         
+        <div v-for="doctor in this.doctorList" v-bind:key="doctor.id"> "This is a doctor: " {{ doctor }} </div>
+
+        <h1 v-if="$store.state.user.authorities[0].name === 'ROLE_DOCTOR'"> Only doctor can Update </h1>
+
+        <offices v-bind:office="office" />
     </div>
 </template>
-
 
 <script>
 
 import OfficeService from '../services/OfficeService';
+import Offices from '../components/Offices.vue';
 
 export default {
     components: {
-
+        Offices,
     },
     data() {
         return {
@@ -33,57 +35,59 @@ export default {
                 officeHours: '',
             },
 
-            doctor:{
-                doctorId : '',
-                officeId : '',
-                firstName : '',
-                lastName : '',
+            doctor: {
+                doctorId: '',
+                officeId: '',
+                firstName: '',
+                lastName: '',
                 specialization: '',
-                costPerHour : '',
+                costPerHour: '',
             },
 
-            officeList:[],
-            
-            doctorList:[]
+            officeList: [],
+
+            doctorList: []
 
         };
     },
     methods: {
-        listOffices() {  
-            OfficeService.getOffices() 
-            .then(response => {
-                if (response.status === 200) {
-                this.office = response.data;
-                this.officeList.unshift( this.office);  
-
-                this.office.forEach(element => { 
-
-                        OfficeService.getDoctorsByOfficeId( element.officeId )
-                        .then(response => {
-                            this.doctor = response.data;                      
-                            this.doctorList.unshift( response.data);         
-                         })  
-                      
-                    });
-
-                  
-             
-                }
-            })                      
-        },         
-        
+        listOffices() {
+            OfficeService.getOffices()
+                .then(response => {
+                    if (response.status === 200) {
+                        this.office = response.data;
+                        this.officeList.unshift(this.office);
+                        this.office.forEach(element => {
+                            OfficeService.getDoctorsByOfficeId(element.officeId)
+                                .then(response => {
+                                    this.doctor = response.data;
+                                    this.doctorList.unshift(response.data);
+                                })
+                        });
+                    }
+                })
+                .catch(error => {
+                    this.handleErrorResponse(error);
+                });
+        },
+        // we need this method to get itemized/separated office things, like office.officeName, office.phoneNumber, etc.
+        getOfficeByOfficeId() {
+            
+        },
+        handleErrorResponse(error) {
+            if (error.response.status == 404) {
+                this.$router.push({ name: 'NotFoundView' });
+            } else {
+                this.isLoading = false;
+                this.$store.commit('SET_NOTIFICATION', `Could not get message data from server.`);
+            }
+        }
     },
-     created() {
+    created() {
         this.listOffices();
-      
-     },
-
-
+    },
 }
 </script>
 
 
-<style>
-
-
-</style>
+<style></style>
