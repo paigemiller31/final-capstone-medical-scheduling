917fb28ad0feb0a1b3cc48117815af4663a1cab3
Shelby Bittner <bittnershelby@gmail.com>
2024-04-15 15:41:08 -0400

    Working on DoctorView

--- vue/src/views/DoctorsView.vue
+++ vue/src/views/DoctorsView.vue
@@ -1,17 +1,64 @@
 <template>
-    <div class="">
+    <body>
+        <div class="home">
+            <div class="form-body">
 
-        ****** List of Doctors available ******
+                <h1 class="main-header">Doctors</h1>
+                <h2> {{ office.officeId }}</h2>
 
-<li>{{ d.firstName }} {{ d.lastName }}</li>
-<!-- <li>{{ d.lastName }}</li> -->
-<li>{{ d.specialization }}</li>
-<li>${{ d.costPerHour }}.00 per hour</li>
+                <div>
+                    <div v-for="o in this.officeList" v-bind:key="o.officeId">
 
-    </div>
+                        <div v-for="d in this.filterDoctors(o.officeId)" v-bind:key="d.doctorId">
+
+                            ****** List of Doctors available ******
+                           
+                            <li>{{ d.firstName }} {{ d.lastName }}</li>
+                            <!-- <li>{{ d.lastName }}</li> -->
+                            <li>{{ d.specialization }}</li>
+                            <li>${{ d.costPerHour }}.00 per hour</li>
+
+                        </div>
+                    </div>
+
+
+
+                    <!-- <ul id="update-button" v-if="$store.state.user.authorities[0].name === 'ROLE_USER'"> -->
+                        ****** For patients to VIEW DOCTORS for OFFICES ******
+                        <li v-for="office in officeList" v-bind:key="office.officeId">
+                            <router-link v-bind:to="{ path: '/offices/' + office.officeId + '/doctors' }">
+                                <button v-bind:officeId="office.officeId">View Doctors for this Office</button>
+                            </router-link>
+                        </li>
+                    <!-- </ul> -->
+
+                
+
+
+
+                    <ul id="update-button" v-if="$store.state.user.authorities[0].name === 'ROLE_DOCTOR'">
+                        ****** For doctors to UPDATE OFFICES ******
+                        <li v-for="office in officeList" v-bind:key="office.officeId">
+                            <router-link v-bind:to="{ path: '/offices/' + office.officeId + '/edit' }">
+                                <button v-bind:officeId="office.officeId">Update</button>
+                            </router-link>
+                        </li>
+                    </ul>
+
+                    <!--
+        <div v-if="$store.state.user.authorities[0].name === 'ROLE_DOCTOR'">
+            <button v-for="office in officeList" v-bind:office="office" v-bind:key="office.officeId" v-on:click.prevent="$router.push('/office/:officeId/edit')">UPDATE</button>
+        </div>
+        -->
+
+                </div>
+            </div>
+        </div>
+    </body>
 </template>
-    
+
 <script>
+
 import OfficeService from '../services/OfficeService';
 
 export default {
@@ -77,11 +124,46 @@ export default {
 
             return ddd;
         },
+        accessOffice(officeId) {
+            OfficeService.getOfficeByOfficeId(officeId)
+                .then(response => {
+                    this.office = response.data
+                })
+        },
     },
     created() {
         this.listOffices();
+        this.accessOffice(this.$route.params.officeId);
     }
 }
 </script>
-    
-<style></style>
+
+
+<style scoped>
+* {
+    font-family: Arial, Helvetica, sans-serif;
+    color: #000000;
+}
+
+body {
+    overflow: hidden;
+}
+
+.main-header {
+    text-transform: uppercase;
+    text-justify: center;
+    text-align: center;
+    padding-top: 25px;
+    font-weight: 100;
+    font-size: 25pt;
+}
+
+.form-body {
+    background-color: rgba(219, 219, 219, 0.5);
+    margin-top: 75px;
+    margin-bottom: 50px;
+    margin-left: 30px;
+    margin-right: 70px;
+    width: 100vh
+}
+</style>
